b03a7b0046d6bd10e8fa79cff051d745d977621a
Freemasoid <freemasoid@icloud.com>
2024-05-02 23:27:39 +0200

    feat: api routes initial setup

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+import { clientRouter } from "./clientRoutes.js";
+import { generalRouter } from "./generalRoutes.js";
+import { mgmtRouter } from "./mgmtRoutes.js";
+import { salesRouter } from "./salesRoutes.js";
+
+export { clientRouter, generalRouter, mgmtRouter, salesRouter };
